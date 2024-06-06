b6f79fe66a1347f661b7c877042026f70be1dbbf
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-06 18:22:25 +0000

    Add login, logout and custom permission
    Add a method field

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,10 +2,17 @@ from rest_framework import serializers
 from .models import Profile
 
 
-
 class ProfileSerializer(serializers.ModelSerializer):
-    model = Profile
-    fields = [
-        'id', 'owner', 'created_at', 'updated_at', 'name',
-        'content', 'image'
-    ]
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image', 'is_owner'
+        ]
