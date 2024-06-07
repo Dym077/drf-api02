838048f3d3e7351ec0995059901813c287f6d1ed
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-07 17:58:59 +0000

    Add a commentdetailserializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -19,3 +19,7 @@ class Meta:
         'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
         'post', 'created_at', 'updated_at', 'content'
     ]    
+
+
+    class CommentDetailSerializer(CommentSerializer):
+        post = serializers.ReadOnlyField(source='post.id')
