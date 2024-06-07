11d2073ff9afb316dda0a6e21e5a0037511bac72
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-07 18:26:14 +0000

    Add commentlist and commentdetaillist

--- comments/serializers.py
+++ comments/serializers.py
@@ -8,18 +8,18 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
-def get_is_owner(self, obj):
-    request = self.context['request']
-    return request.user == obj.owner
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
 
-class Meta:
-    model = Comment
-    fields = [
-        'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-        'post', 'created_at', 'updated_at', 'content'
-    ]    
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
+        ]    
 
 
-    class CommentDetailSerializer(CommentSerializer):
-        post = serializers.ReadOnlyField(source='post.id')
+class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')
