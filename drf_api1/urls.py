a72aecb1f3d69f80fc3b8f0a25b303cc8c8bb0b8
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-09 16:12:23 +0000

    Tidy up API w pagination and root route
    Add naturaltime

--- drf_api1/urls.py
+++ drf_api1/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
