3e3f7848418245ca653997a26ece6aa8dbd8f9da
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-09 10:39:01 +0000

    Add extra fields to Posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,33 +9,25 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image size larger than 2MB'
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
                 'Image width larger than 4096px!'
-            )  
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'    
             )
         return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-
-    class Meta:
-        model = Post
-        fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter'
-        ]
 
     def get_like_id(self, obj):
         user = self.context['request'].user
@@ -52,5 +44,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
-            'like_id',
-        ]    
+            'like_id', 'likes_count', 'comments_count',
+        ]
