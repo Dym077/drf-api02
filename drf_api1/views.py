dc61e4702ae6bbe11a650eaa68a17e1ced47d7cf
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-10 14:03:11 +0000

    Change welcome message in api view

--- drf_api1/views.py
+++ drf_api1/views.py
@@ -5,5 +5,5 @@ from rest_framework.response import Response
 @api_view()
 def root_route(request):
     return Response({
-        "message": "This is the drf API."
+        "message": "This is the DRF API for the Virtual Gallery Website."
     })
