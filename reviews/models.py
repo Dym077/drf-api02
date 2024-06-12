a4a5511477bde296894c75252f393e9bc5551cbe
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-12 09:10:20 +0000

    Create reviews model
    Add serializer fore reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Review(models.Model):
+    """
+    Review model, related to User and Post
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    review = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
