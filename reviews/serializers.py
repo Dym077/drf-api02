19f00a58d06d387304d01a018443635d2b50d5e3
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-13 15:42:31 +0000

    Correct commentdetailserializer

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -30,29 +30,8 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class ReviewDetailSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    created_at = serializers.SerializerMethodField()
-    updated_at = serializers.SerializerMethodField()
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-
-    def get_created_at(self, obj):
-        return naturaltime(obj.created_at)
-
-    def get_updated_at(self, obj):   
-        return naturaltime(obj.updated_at)  # Note: Updated to use obj.updated_at
-
-    class Meta:
-        model = Review
-        fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'created_at', 'updated_at', 'content'
-        ]
+    
+    artist = serializers.ReadOnlyField(source='artist.id')
 
 
 class CommentSerializer(serializers.ModelSerializer):
