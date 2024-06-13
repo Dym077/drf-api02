422dca3be13329998f0c9e54fc0e3c62b299fa86
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-13 14:32:35 +0000

    Modify reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -8,7 +8,6 @@ class Review(models.Model):
     Review model, related to User and Post
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     artist = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
