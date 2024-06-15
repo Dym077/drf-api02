c24a9558e65abd18b7492ffa3aeba8ba0af1c6e5
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-15 19:43:24 +0000

    Update migrations

--- drf_api1/settings.py
+++ drf_api1/settings.py
@@ -161,20 +161,7 @@ else:
 #     }
 # }
 
-DATABASES = {
 
-    'default': ({
-
-        'ENGINE': 'django.db.backends.sqlite3',
-
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-    } if 'DEV' in os.environ else dj_database_url.parse(
-
-        os.environ.get('DATABASE_URL')
-
-    ))
-}
 
 
 # Password validation
