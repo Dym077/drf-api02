73eaba6d9bdbe08b8166d290e5f7900e83710db4
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-07-03 14:50:30 +0000

    Running new migrations

--- drf_api1/settings.py
+++ drf_api1/settings.py
@@ -155,7 +155,7 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-    print('connected')
+    print('connected to database')
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
