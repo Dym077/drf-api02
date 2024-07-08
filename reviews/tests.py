10d3165f07383375f2ded82f17c724a76a459d50
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-07-08 19:18:06 +0000

    Make Review test

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here
+from reviews import Review
+
+class TestReviewForm(TestCase):
+
+    def test_form_is_valid(self):
+        review_form = Review({'content': 'This is a great post'})
+        self.assertTrue(review_form.is_valid())
